05538c1a4911df49760ef30291b2eef42b5bd7cc
SpencerBlythe <121988330+SpencerBlythe@users.noreply.github.com>
2023-05-02 10:23:44 -0400

    Update Pokematch.js

--- Pokematch/Pokematch.js
+++ Pokematch/Pokematch.js
@@ -26,45 +26,38 @@ window.onload = function() {
     shuffleCards();
     startGame();
 }
-
 function shuffleCards() {
-    cardSet = cardList.concat(cardList); //two of each card
+    cardSet = cardList.concat(cardList); 
     console.log(cardSet);
-    //shuffle
+    
     for (let i = 0; i < cardSet.length; i++) {
-        let j = Math.floor(Math.random() * cardSet.length); //get random index
-        //swap
+        let j = Math.floor(Math.random() * cardSet.length); 
+        
         let temp = cardSet[i];
         cardSet[i] = cardSet[j];
         cardSet[j] = temp;
     }
     console.log(cardSet);
 }
-
 function startGame() {
-    //arrange the board 4x5
     for (let r = 0; r < rows; r++) {
         let row = [];
         for (let c = 0; c < columns; c++) {
             let cardImg = cardSet.pop();
-            row.push(cardImg); //JS
-
-            // <img id="0-0" class="card" src="water.jpg">
+            row.push(cardImg); 
+>
             let card = document.createElement("img");
             card.id = r.toString() + "-" + c.toString();
             card.src = cardImg + ".jpg";
             card.classList.add("card");
             card.addEventListener("click", selectCard);
             document.getElementById("board").append(card);
-
         }
         board.push(row);
     }
-
     console.log(board);
     setTimeout(hideCards, 1000);
 }
-
 function hideCards() {
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < columns; c++) {
@@ -73,14 +66,13 @@ function hideCards() {
         }
     }
 }
-
 function selectCard() {
 
     if (this.src.includes("back")) {
         if (!card1Selected) {
             card1Selected = this;
 
-            let coords = card1Selected.id.split("-"); //"0-1" -> ["0", "1"]
+            let coords = card1Selected.id.split("-"); 
             let r = parseInt(coords[0]);
             let c = parseInt(coords[1]);
 
@@ -89,7 +81,7 @@ function selectCard() {
         else if (!card2Selected && this != card1Selected) {
             card2Selected = this;
 
-            let coords = card2Selected.id.split("-"); //"0-1" -> ["0", "1"]
+            let coords = card2Selected.id.split("-");
             let r = parseInt(coords[0]);
             let c = parseInt(coords[1]);
 
@@ -97,11 +89,8 @@ function selectCard() {
             setTimeout(update, 1000);
         }
     }
-
 }
-
 function update() {
-    //if cards aren't the same, flip both back
     if (card1Selected.src != card2Selected.src) {
         card1Selected.src = "back.jpg";
         card2Selected.src = "back.jpg";
